685324d610e3d9a268ee3211b6b4ea52c45b795c
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-08-09 11:29:14 -0400

    refactoring

--- scripts/the_golden_key.py
+++ scripts/the_golden_key.py
@@ -1,3 +1,17 @@
-from booknlp.booknlp import BookNLP
-
 if __name__ == '__main__':
+    from booknlp.booknlp import BookNLP
+
+    model_params = {
+        "pipeline": "entity,quote,supersense,event,coref",
+        "model": "big"
+    }
+
+    booknlp = BookNLP("en", model_params)
+
+    input_file = "data/harry_potter_cleaned.txt"
+
+    output_directory = "data/harry_potter"
+
+    book_id = "harry_potter"
+
+    booknlp.process(input_file, output_directory, book_id)
